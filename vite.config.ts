2c6b885de3e4de603e260020fbb99de95837a37b
velikoss <velikoss@vk.com>
2025-04-04 18:31:14 +0300

    host

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit(), Terminal()],
 	optimizeDeps: {
+	},
+	server: {
+		allowedHosts: ['xn--80adsi2a0f.xn--b1agjiduva.xn--p1ai'],
 	}
 });
